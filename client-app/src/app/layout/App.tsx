cd616f2c4784e441ef963b1f1cee80c7a362159b
Randy Fair <hrf@gmail.com>
2021-04-11 20:58:30 -0400

    End of Section 6

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,33 +1,42 @@
 import { Fragment, useEffect, useState } from 'react';
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import {v4 as uuid} from 'uuid';
-
+import { v4 as uuid } from 'uuid';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [selectedActivity, setSelectedActivity] = useState<
+    Activity | undefined
+  >(undefined);
   const [editMode, setEditMode] = useState(false);
-
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then((response) => {
-      setActivities(response.data);
+    agent.Activities.list().then((response) => {
+      let activities: Activity[] = [];
+      response.forEach((activity) => {
+        activity.date = activity.date.split('T')[0];
+        activities.push(activity);
+      });
+      setActivities(activities);
+      setLoading(false);
     });
   }, []);
 
   function handleSelectActivity(id: string) {
-    setSelectedActivity(activities.find(x => x.id === id));
+    setSelectedActivity(activities.find((x) => x.id === id));
   }
 
   function handleCancelSelectActivity() {
     setSelectedActivity(undefined);
   }
 
-  function handleFormOpen(id?:string) {
+  function handleFormOpen(id?: string) {
     id ? handleSelectActivity(id) : handleCancelSelectActivity();
     setEditMode(true);
   }
@@ -37,23 +46,45 @@ function App() {
   }
 
   function handleDeleteActivity(id: string) {
-    setActivities([...activities.filter(x => x.id !== id)]);
+    setSubmitting(true);
+    agent.Activities.delete(id).then(() => {
+      setActivities([...activities.filter((x) => x.id !== id)]);
+      setSubmitting(false);
+    })
+    
   }
 
   function handleCreateOrEditActivity(activity: Activity) {
-    activity.id 
-    ? setActivities([...activities.filter(x => x.id !== activity.id), activity]) 
-    : setActivities([...activities, {...activity, id: uuid()}]);
-    setEditMode(false);
-    setSelectedActivity(activity);
+    setSubmitting(true);
+    if (activity.id) {
+      agent.Activities.update(activity).then(() => {
+        setActivities([
+          ...activities.filter((x) => x.id !== activity.id),
+          activity,
+        ]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      });
+    } else {
+      activity.id = uuid();
+      agent.Activities.create(activity).then(() => {
+        setActivities([...activities, activity]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      });
+    }
   }
 
+  if (loading) return <LoadingComponent content='Loading app' />;
+
   return (
-    <Fragment >
-      <NavBar openForm={handleFormOpen}/>
-      <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard 
-          activities={activities} 
+    <Fragment>
+      <NavBar openForm={handleFormOpen} />
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashboard
+          activities={activities}
           selectedActivity={selectedActivity}
           selectActivity={handleSelectActivity}
           cancelSelectActivity={handleCancelSelectActivity}
@@ -62,9 +93,9 @@ function App() {
           closeForm={handleFormClose}
           createOrEdit={handleCreateOrEditActivity}
           deleteActivity={handleDeleteActivity}
+          submitting={submitting}
         />
       </Container>
-
     </Fragment>
   );
 }
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,12 +6,14 @@ interface Props {
   activity: Activity | undefined;
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
+  submitting: boolean;
 }
 
 export default function ActivityForm({
   activity: selectedActivity,
   closeForm,
-  createOrEdit
+  createOrEdit,
+  submitting,
 }: Props) {
   const initialState = selectedActivity ?? {
     id: '',
@@ -22,32 +24,81 @@ export default function ActivityForm({
     sponsor: '',
     poc: '',
     city: '',
-    venue: ''
-  }
+    venue: '',
+  };
 
   const [activity, setActivity] = useState(initialState);
 
   function handleSubmit() {
-      createOrEdit(activity);
+    createOrEdit(activity);
   }
 
-  function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-    const {name, value} = event.target;
-    setActivity({...activity, [name]: value})
-}
+  function handleInputChange(
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    const { name, value } = event.target;
+    setActivity({ ...activity, [name]: value });
+  }
 
   return (
     <Segment clearing>
       <Form onSubmit={handleSubmit} autoComplete='off'>
-        <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange} />
-        <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={handleInputChange}/>
-        <FormInput placeholder='Category' value={activity.category} name='category' onChange={handleInputChange}/>
-        <FormInput placeholder='Date' value={activity.date} name='date' onChange={handleInputChange}/>
-        <FormInput placeholder='Sponsor' value={activity.sponsor} name='sponsor' onChange={handleInputChange}/>
-        <FormInput placeholder='POC' value={activity.poc} name='poc' onChange={handleInputChange}/>
-        <FormInput placeholder='City' value={activity.city} name='city' onChange={handleInputChange}/>
-        <FormInput placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange}/>
-        <Button floated='right' positive type='submit' content='Submit' />
+        <Form.Input
+          placeholder='Title'
+          value={activity.title}
+          name='title'
+          onChange={handleInputChange}
+        />
+        <Form.TextArea
+          placeholder='Description'
+          value={activity.description}
+          name='description'
+          onChange={handleInputChange}
+        />
+        <FormInput
+          placeholder='Category'
+          value={activity.category}
+          name='category'
+          onChange={handleInputChange}
+        />
+        <FormInput
+          type='date'
+          placeholder='Date'
+          value={activity.date}
+          name='date'
+          onChange={handleInputChange}
+        />
+        <FormInput
+          placeholder='Sponsor'
+          value={activity.sponsor}
+          name='sponsor'
+          onChange={handleInputChange}
+        />
+        <FormInput
+          placeholder='POC'
+          value={activity.poc}
+          name='poc'
+          onChange={handleInputChange}
+        />
+        <FormInput
+          placeholder='City'
+          value={activity.city}
+          name='city'
+          onChange={handleInputChange}
+        />
+        <FormInput
+          placeholder='Venue'
+          value={activity.venue}
+          name='venue'
+          onChange={handleInputChange}
+        />
+        <Button
+          loading={submitting}
+          floated='right'
+          positive
+          type='submit'
+          content='Submit'
+        />
         <Button
           onClick={closeForm}
           floated='right'
@@ -58,5 +109,3 @@ export default function ActivityForm({
     </Segment>
   );
 }
-
-
