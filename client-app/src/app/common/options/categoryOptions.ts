7d93fb2342b8165cc8c6ebcc8fe958aae212108d
Randy Fair <hrf@gmail.com>
2021-04-23 22:28:32 -0400

    End of Section 11

---
+++ client-app/src/app/common/options/categoryOptions.ts
@@ -0,0 +1,8 @@
+export const categoryOptions = [
+    {text: 'Youth Camp', value: 'camp'},
+    {text: 'Gospel Meeting', value: 'gospel'},
+    {text: 'Information', value: 'info'},
+    {text: 'Youth Devo', value: 'devo'},
+    {text: 'Singing', value: 'singing'},
+    {text: 'Ladies Meetings', value: 'ladies'}
+]
